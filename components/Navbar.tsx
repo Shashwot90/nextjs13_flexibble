8e8ec8b5dd3c628456901c6e2b11904f01c5c869
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-07-26 13:48:10 +0545

    authentication complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { NavLinks } from '@/constants'
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from '@/lib/session';
+import { signOut } from 'next-auth/react';
+import ProfileMenu from './ProfileMenu';
 
-const Navbar = () => {
-    const session = null;
+const Navbar = async () => {
+    const session = await getCurrentUser();
 
   return (
     <nav className='flexBetween navbar'>
@@ -24,10 +27,24 @@ const Navbar = () => {
             </ul>
         </div>
         <div className="flexCenter gap-4">
-            {session ? (
+            {session?.user ? (
                 <>
-                    UserPhoto 
+                <ProfileMenu session={session} />
+                {/* {session?.user?.image && 
+                (
+                    <Link href={`/profile/${session?.user?.id}`}>
+                        <Image 
+                        src={session.user.image}
+                        width={40}
+                        height={40}
+                        className='rounded-full'
+                        alt={session.user.name} /> 
+                    </Link>
+                )} */}
+                    
                     <Link href="/create-project">Share Work</Link>
+
+                    {/* <button type='button' className="text-sm" onClick={signOut}>Sign Out</button> */}
                 </>
             ): (
                 <AuthProviders />
