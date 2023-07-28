2309481fd5a5d195f2ef2addc5d90ea1b9ba298e
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-07-28 15:24:47 +0545

    create project complete

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -6,6 +6,8 @@ import FormField from "./FormField";
 import { categoryFilters } from "@/constants";
 import CustomMenu from "./CustomMenu";
 import Button from "./Button";
+import { CreateNewProject, fetchToken } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 type Props = {
     type: string,
@@ -13,18 +15,25 @@ type Props = {
 }
 
 const ProjectForm = ({ type, session }: Props) => {
+  const router = useRouter();
 
-  const handleFormSubmit = (e: React.FormEvent) => {
+  const handleFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     setIsSubmitting(true);
 
+    const { token } = await fetchToken();
+
     try {
         if(type === 'create') {
-            
+            await CreateNewProject(form, session?.user?.id, token);
+
+            router.push('/');
         }
     } catch (error) {
-        
+        console.log(error);
+    } finally {
+        setIsSubmitting(false);
     }
   };
 
