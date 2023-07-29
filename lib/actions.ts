c62251cbd1a541a20ee62944380110f5a89f32df
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-07-29 16:28:43 +0545

    project card

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 import { ProjectForm } from "@/common.types";
-import { createProjectMutation, createUserMutation, getUserQuery } from "@/graphql";
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from "@/graphql";
 import { GraphQLClient } from "graphql-request";
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -74,4 +74,10 @@ export const CreateNewProject = async (form: ProjectForm, creatorId: string, tok
         }
         return makeGraphQLRequest(createProjectMutation, variables)
     }
+}
+
+export const fetchAllProjects = async (category?: string, endCursor?: string) => {
+    client.setHeader('x-api-key', apiKey);
+
+    return makeGraphQLRequest(projectsQuery, { category, endCursor })
 }
